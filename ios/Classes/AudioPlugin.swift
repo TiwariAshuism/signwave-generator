fe456d460af2e94a6bac14dace023321537d8a9e
Ashu <aksamtiwari@gmail.com>
2023-12-08 16:10:59 +0530

    fix: ios audio file

--- ios/Classes/AudioPlugin.swift
+++ ios/Classes/AudioPlugin.swift
@@ -2,9 +2,10 @@ import Flutter
 import AVFoundation
 
 public class AudioPlugin: NSObject, FlutterPlugin {
-    
-    private var audioPlayer: AVAudioPlayer?
-    
+
+    private var audioEngine: AVAudioEngine?
+    private var playerNode: AVAudioPlayerNode?
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "audio_plugin", binaryMessenger: registrar.messenger())
         let instance = AudioPlugin()
@@ -31,41 +32,50 @@ public class AudioPlugin: NSObject, FlutterPlugin {
     }
 
     private func startAudio(frequency: Double) {
-        guard audioPlayer == nil else {
-            stopAudio()
-            return
-        }
-        
+        stopAudio()
+
+        audioEngine = AVAudioEngine()
+        playerNode = AVAudioPlayerNode()
+        audioEngine?.attach(playerNode!)
+
+        let format = AVAudioFormat(standardFormatWithSampleRate: 44100, channels: 1)!
+        audioEngine?.connect(playerNode!, to: audioEngine!.mainMixerNode, format: format)
+
+        let sampleRate = 44100
         let period = 1.0 / frequency
         let amplitude: Float = 0.5
         let duration = 1.0
-        
-        let sampleRate = 44100
         let numberOfFrames = Int(duration * Double(sampleRate))
-        
+
         var samples = [Float]()
         for frame in 0..<numberOfFrames {
             let value = (frame % Int(period * Double(sampleRate))) < Int(period * Double(sampleRate)) / 2 ? amplitude : -amplitude
             samples.append(value)
         }
-        
-        let format = AVAudioFormat(standardFormatWithSampleRate: Double(sampleRate), channels: 1)!
+
         let buffer = AVAudioPCMBuffer(pcmFormat: format, frameCapacity: AVAudioFrameCount(numberOfFrames))!
-        buffer.floatChannelData![0].withMemoryRebound(to: Float.self, capacity: numberOfFrames) { ptr in
-            memcpy(ptr, samples, numberOfFrames * MemoryLayout<Float>.size)
+        buffer.frameLength = AVAudioFrameCount(numberOfFrames)
+        for channel in 0..<Int(format.channelCount) {
+            let channelData = buffer.floatChannelData![channel]
+            for frame in 0..<numberOfFrames {
+                channelData[frame] = samples[frame]
+            }
         }
-        
-        audioPlayer = AVAudioPlayer()
-        audioPlayer?.prepare(toPlay: true)
-        audioPlayer?.numberOfLoops = -1 // Loop indefinitely
-        audioPlayer?.enableRate = true
-        audioPlayer?.rate = Float(frequency)
-        audioPlayer?.scheduleBuffer(buffer, at: nil, options: .loops, completionHandler: nil)
-        audioPlayer?.play()
+
+        playerNode?.scheduleBuffer(buffer, completionHandler: nil)
+        audioEngine?.prepare()
+
+        do {
+            try audioEngine?.start()
+            playerNode?.play()
+        } catch {
+            print("Error starting audio engine: \(error.localizedDescription)")
+        }
     }
 
     private func stopAudio() {
-        audioPlayer?.stop()
-        audioPlayer = nil
+        playerNode?.stop()
+        audioEngine?.stop()
+        audioEngine?.reset()
     }
 }
